cbb0bcf19717b180445933e1c37f348cbf4af82d
Diego Braga <braga.diego71@gmail.com>
2022-12-19 23:07:04 -0300

    Construindo a secao de troca de persoangens JS

--- SiteV2/script.js
+++ SiteV2/script.js
@@ -23,5 +23,49 @@ function showSlides(n) {
   }
 
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  // dots[slideIndex-1].className += " active";
 }
+
+/*Selecao de caracteristicas dos personagens*/
+
+/*Capturando o perfil selecionado*/
+
+// const btnProfile = [...document.querySelectorAll('.btn-personagens-descricao')]
+
+// console.log(btnProfile)
+
+
+
+// const slctPersona =document.querySelector('.btn-personagens-descricao')
+const slctPersonagens =[...document.querySelectorAll('.btn-personagens-descricao')]
+
+function retornaProfile(slctPersonagens) {
+  return slctPersonagens.classList[1]
+}
+
+const t1 = slctPersonagens.map(retornaProfile)
+console.log(t1)
+
+
+slctPersona.addEventListener('click',()=>{
+
+    let btnProfile = slctPersona.classList[1]
+    console.log(btnProfile)
+
+    switch (btnProfile){
+    case 'Rintaro_Profile':
+        let desRitaro = document.querySelector('.description1').innerHTML = ''
+        
+        console.log('Personagem selecionado' + desRitaro)
+        break
+
+    case 'Kurisu_profile':
+        let desKurisu = document.querySelector('.description2').innerHTML = 'Kurisu Makise (牧瀬紅莉栖Makise Kurisu ? ) é filha de Shouichi Makise , uma garota genial que se formou na universidade aos dezessete anos, e membro do Brain Science Institute da Viktor Chondria University e do Future Gadget Lab .'
+
+        console.log('Personagem selecionado' + desKurisu)
+        break
+    default:
+        console.log('Nao Encontrado')
+}
+
+})
